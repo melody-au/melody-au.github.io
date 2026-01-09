f9bbe3e637c6b16c3749b0bec6725b2a686d0c4f
Clarissa Au <clarissayuenyee@gmail.com>
2026-01-10 04:58:19 +0800

    tailwind / vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.chiiika.com',
+  site: 'https://www.chiiika.com',
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [vue()]
 });
